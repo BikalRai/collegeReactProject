e5c3c615587ef386dcf56ecdb364a23511af53bf
Bikal Rai <bikalrai2785@gmail.com>
2025-08-20 17:21:02 +0545

    rework category section to be responsive

--- src/components/categorySection'/CategorySection.tsx
+++ src/components/categorySection'/CategorySection.tsx
@@ -1,36 +1,36 @@
-import { CiDesktopMouse1 } from 'react-icons/ci';
-import { GiProcessor } from 'react-icons/gi';
-import { LuMonitor } from 'react-icons/lu';
-import { PiOfficeChair } from 'react-icons/pi';
-import type { CategoryTypes } from '../../utilities/types/categoryTypes';
-import CategoryCard from './CategoryCard';
+import { CiDesktopMouse1 } from "react-icons/ci";
+import { GiProcessor } from "react-icons/gi";
+import { LuMonitor } from "react-icons/lu";
+import { PiOfficeChair } from "react-icons/pi";
+import type { CategoryTypes } from "../../utilities/types/categoryTypes";
+import CategoryCard from "./CategoryCard";
 
 const categories: CategoryTypes[] = [
   {
     id: 1,
-    title: 'Peripherals',
-    icon: <CiDesktopMouse1 className="w-12 h-12 stroke-1" />,
+    title: "Peripherals",
+    icon: <CiDesktopMouse1 className='w-12 h-12 stroke-1' />,
   },
   {
     id: 2,
-    title: 'Visuals',
-    icon: <LuMonitor className="w-12 h-12 stroke-1" />,
+    title: "Visuals",
+    icon: <LuMonitor className='w-12 h-12 stroke-1' />,
   },
   {
     id: 3,
-    title: 'Power Core',
-    icon: <GiProcessor className="w-12 h-12 stroke-1" />,
+    title: "Power Core",
+    icon: <GiProcessor className='w-12 h-12 stroke-1' />,
   },
   {
     id: 4,
-    title: 'Comfort Gear',
-    icon: <PiOfficeChair className="w-12 h-12 stroke-1" />,
+    title: "Comfort Gear",
+    icon: <PiOfficeChair className='w-12 h-12 stroke-1' />,
   },
 ];
 
 const CategorySection = () => {
   return (
-    <div className="flex justify-center items-center gap-20 mt-20">
+    <div className=' grid grid-cols-2 justify-items-center md:flex md:justify-center md:items-center gap-20 mt-20'>
       {categories.map((category) => (
         <CategoryCard key={category.id} category={category} />
       ))}
