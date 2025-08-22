c50611dcb097dd8d73cdca94a25b8ad68ce4425f
Bikal Rai <bikalrai2785@gmail.com>
2025-08-22 22:40:26 +0545

    add cart card

---
+++ src/components/cart/CartCard.tsx
@@ -0,0 +1,21 @@
+import QuantityCounter from "../QuantityCounter";
+
+const CartCard = () => {
+  return (
+    <div className='flex gap-5 bg-cardbg p-4 rounded-2xl'>
+      <div className='h-24 w-24 border rounded-xl overflow-hidden'>
+        <img src='' alt='' />
+      </div>
+      <div className='grow flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between'>
+        <h4>Product name</h4>
+        <div className='flex items-center justify-between lg:flex-col'>
+          <p>Rs product price</p>
+          <p className='line-through text-muted'>Rs discounted price</p>
+        </div>
+        <QuantityCounter />
+      </div>
+    </div>
+  );
+};
+
+export default CartCard;
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,5 @@
+import CartCard from "../components/cart/CartCard";
 import AppContainer from "../components/container/AppContainer";
-import QuantityCounter from "../components/QuantityCounter";
 import SectionHeaderNoLinks from "../components/SectionHeaderNoLinks";
 import AppLayout from "../layouts/AppLayout";
 import type { SectionHeaderNoLinkTypes } from "../utilities/types/appTypes";
@@ -14,19 +14,7 @@ const CartPage = () => {
       <AppContainer>
         <div>
           <SectionHeaderNoLinks section={section} />
-          <div className='flex gap-5 bg-cardbg p-4 rounded-2xl'>
-            <div className='h-24 w-24 border rounded-xl overflow-hidden'>
-              <img src='' alt='' />
-            </div>
-            <div className='grow flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between'>
-              <h4>Product name</h4>
-              <div className='flex items-center justify-between lg:flex-col'>
-                <p>Rs product price</p>
-                <p className='line-through text-muted'>Rs discounted price</p>
-              </div>
-              <QuantityCounter />
-            </div>
-          </div>
+          <CartCard />
         </div>
       </AppContainer>
     </AppLayout>
