2ff0abb7275050aca59f2a343204aeb9438625e9
Bikal Rai <bikalrai2785@gmail.com>
2025-08-22 22:57:53 +0545

    rework cart card

--- src/components/cart/CartCard.tsx
+++ src/components/cart/CartCard.tsx
@@ -2,17 +2,17 @@ import QuantityCounter from "../QuantityCounter";
 
 const CartCard = () => {
   return (
-    <div className='flex gap-5 bg-cardbg p-4 rounded-2xl'>
+    <div className='flex gap-5 bg-cardbg p-4 rounded-xl '>
       <div className='h-24 w-24 border rounded-xl overflow-hidden'>
         <img src='' alt='' />
       </div>
-      <div className='grow flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between'>
+      <div className='grow flex flex-col gap-2 lg:flex-row lg:items-center lg:justify-between md:ps-48'>
         <h4>Product name</h4>
-        <div className='flex items-center justify-between lg:flex-col'>
+        <div className='grow flex items-center justify-between lg:flex-col'>
           <p>Rs product price</p>
           <p className='line-through text-muted'>Rs discounted price</p>
         </div>
-        <div className='ms-auto'>
+        <div className='ms-auto w-fit'>
           <QuantityCounter />
         </div>
       </div>
