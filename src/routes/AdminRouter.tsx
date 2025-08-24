45b2e368ab88ae6394a29b657bf69a4ceaa9f973
Bikal Rai <bikalrai2785@gmailcom>
2025-08-24 13:33:49 +0545

    some changes

--- src/routes/AdminRouter.tsx
+++ src/routes/AdminRouter.tsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import AdminDashboard from "../pages/AdminDashboard";
-import AdminUserDetailspage from "../pages/AdminUserDetailspage";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AdminDashboard from '../pages/AdminDashboard';
+import AdminUserDetailspage from '../pages/AdminUserDetailspage';
 
 const router = createBrowserRouter([
-  { path: "/", element: <AdminDashboard /> },
-  { path: "/users", element: <AdminUserDetailspage /> },
+  { path: '/', element: <AdminDashboard /> },
+  { path: '/users', element: <AdminUserDetailspage /> },
 ]);
 
 const AdminRouter = () => {
